5c081807d57a786c80abc60d91e50c313695d3ef
alienpingu <42205141+alienpingu@users.noreply.github.com>
2019-09-30 20:10:37 +0200

    Oggi ho lavorato troppo

--- Full Stack Web Developer/Resume/2 - 0 to Perfect/script.js
+++ Full Stack Web Developer/Resume/2 - 0 to Perfect/script.js
@@ -1,5 +1,5 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// When the user scrolls the page, execute addSticky
+window.onscroll = function() {addSticky()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -8,10 +8,11 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function addSticky() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
