bbea461c23ce8af1d54abce21900a545d5826536
alienpingu <42205141+alienpingu@users.noreply.github.com>
2019-10-04 14:25:32 +0200

    upload reposity

--- Full Stack Web Developer/Resume/2 - 0 to Perfect/script.js
+++ Full Stack Web Developer/Resume/2 - 0 to Perfect/script.js
@@ -1,18 +1,9 @@
-// When the user scrolls the page, execute addSticky
-window.onscroll = function() {addSticky()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function addSticky() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+function Tendina() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    navbar.classList.remove("sticky");
+    x.className = "topnav";
   }
-}
+} 
 
