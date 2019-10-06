fb46e641688eaa24b309ddf0fc67936086f48d48
alienpingu <42205141+alienpingu@users.noreply.github.com>
2019-10-06 12:21:25 +0200

    starting mobile first
    Mi sono rassgnato

--- Full Stack Web Developer/Resume/2 - 0 to Perfect/script.js
+++ Full Stack Web Developer/Resume/2 - 0 to Perfect/script.js
@@ -1,3 +1,21 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var myTopnav = document.getElementById("myTopnav");
+
+// Get the offset position of the myTopnav
+var sticky = myTopnav.offsetTop;
+
+// Add the sticky class to the myTopnav when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    myTopnav.classList.add("sticky")
+  } else {
+    myTopnav.classList.remove("sticky");
+  }
+} 
+
 function Tendina() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,3 +24,4 @@ function Tendina() {
     x.className = "topnav";
   }
 } 
+
