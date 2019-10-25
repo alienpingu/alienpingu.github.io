74c7917ceeb22b04aa379cbec8f20f096c953506
alienpingu <42205141+alienpingu@users.noreply.github.com>
2019-10-25 10:47:44 +0200

    upload new smarlek
    add animation

--- Full Stack Web Developer/smarlek eu/script.js
+++ Full Stack Web Developer/smarlek eu/script.js
@@ -1,9 +1,1 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+AOS.init();
