e31919e8c03aeee519a1bd4f10996e92e355b21b
alienpingu <42205141+alienpingu@users.noreply.github.com>
2019-11-01 15:24:49 +0100

    upload puttini.html
    ok Contatti card
    ok IlParco foto
    ok navigation
    ok footer

--- Full Stack Web Developer/Puttini/script.js
+++ Full Stack Web Developer/Puttini/script.js
@@ -8,3 +8,17 @@ function openNav() {
 function closeNav() {
   document.getElementById("MobileNav").style.width = "0%";
 } 
+
+// Collapsible Gallery
+var g = document.getElementById('Gallery');
+
+function dSwitch() {
+	if (g.classList.contains('d-none') == true) {
+		g.classList.remove('d-none');
+	}
+	else {
+		g.classList.add('d-none');
+	}
+}
+
+
