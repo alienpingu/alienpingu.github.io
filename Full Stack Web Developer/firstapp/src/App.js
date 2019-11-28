b125cfa6ef14b9c610dc048f06f588bc1c830c68
alienpingu <samuele.bertocco@gmail.com>
2019-11-28 17:08:10 +0100

    Upload working first React App!

--- Full Stack Web Developer/firstapp/src/App.js
+++ Full Stack Web Developer/firstapp/src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import './App.css';
-import 'tachyons';
-function App() {
-  return (
-    <div className="App bg-red">
-      	<article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10 grow">
-		  	<div className="tc">
-		    	<img src="https://robohash.org/1" className="br-100 h4 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you" alt='Cat'></img>
-		    	<h1 className="f3 mb2">Robored W.</h1>
-		    	<h2 className="f5 fw4 gray mt0">CRO (Chief Rob Officer)</h2>
-		  </div>
-		</article>
-    </div>
-  );
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+	
+	componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({robots: users})});
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
 }
 
-export default App;
+
+export default App; 
--- Full Stack Web Developer/firstapp/src/Card.js
+++ Full Stack Web Developer/firstapp/src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({name, email, id}) => { return (
 
-      	<div className="mw5 tc center bg-white br3 pa3 pa4-ns mv3 ba b--black-10 grow">
-      		<img className='br-100 h4 w4 dib ba b--black-05 pa2' alt='pic' src={`https://robohash.org/${id}`}/>
+      	<div className="fl w5 ma1 bg-washed-blue ba br3 grow">
+      		<img className='br-100 h4 w4 dib ba b-white-05' alt='pic' src={`https://robohash.org/${id}?300x300`}/>
       		<h1 className='f3 mb2'>{name}</h1>
       		<p className='f5 fw4 gray mt0'>{email}</p>
       	</div>
