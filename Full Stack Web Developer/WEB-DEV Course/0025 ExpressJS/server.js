5016de21417a3ee071f39ee0328628b1ba6a4dad
alienpingu <samuele.bertocco@gmail.com>
2019-12-16 10:01:07 +0100

    Upload Node and Express

---
+++ Full Stack Web Developer/WEB-DEV Course/0025 ExpressJS/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+// Response of text/html
+app.get('/', (req, res) => {
+	res.send('<h1>Hello ExpressJS!</h1>')
+});
+
+// Response of JSON
+app.get('/user', (req, res) => {
+	const user = {
+		name: 'Samu',
+		city: 'Milan'
+	}
+	res.send(user);
+});
+
+app.listen(3000);
