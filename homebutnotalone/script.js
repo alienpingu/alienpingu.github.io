a61d24043751c339f1e0485ac74713d4e57a16a3
AlienPingu <42205141+alienpingu@users.noreply.github.com>
2020-11-12 13:51:02 +0100

    Add files via upload

--- homebutnotalone/script.js
+++ homebutnotalone/script.js
@@ -1,8 +1,45 @@
 // 6/11/2020 - Samuele Bertocco
 
-$('p.cta').each((index, element) => element.addEventListener('click', () => $('#hover-wrap').css('display', 'flex')))
+// ScrollInto the view
+scrollTo = (id) => $(`#${id}`)[0].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+// al click su arrow bounce scrolla la vista su sezione info
+$('.arrow').click(() => scrollTo('info'))
 
+
+// Navbar
+// Hide navbar when scroll down
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+
+    var currentScrollPos = window.pageYOffset;
+    (prevScrollpos > currentScrollPos) ? $('nav').css('top', '0') : $('nav').css('top', '-64px') 
+    prevScrollpos = currentScrollPos;
+
+} 
+
+// Products
+// al click sul pulsante della carta mostra l' hover div
+$('p.cta').each((index, element) => element.addEventListener('click', () => $('#hover-wrap').css('display', 'flex')))
+// Al click sull' ombra nascondi l'hover div
 $('#hover-wrap')[0].addEventListener('click', () => $('#hover-wrap').css('display', 'none'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 console.log(`
 ───────────────────────────────
