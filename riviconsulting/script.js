a57bd71ceddf9371cba2ae23f24aee4fbef688cb
AlienPingu <42205141+alienpingu@users.noreply.github.com>
2020-12-02 19:39:32 +0100

    Add files via upload

--- riviconsulting/script.js
+++ riviconsulting/script.js
@@ -1,1 +1,19 @@
+let navBar = $('nav.navbar')
+let screenW = screen.width
+
+$('#heroBtn0').on('click', () => window.open("tel:390221119572"))
+$('#heroBtn1').on('click', () => $('#footer')[0].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})) 
+
+// Navbar script
+// Hide navbar when scroll down
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    (prevScrollpos > currentScrollPos) ? navBar.css('top', '0') : navBar.css('top', '-70px') 
+    prevScrollpos = currentScrollPos;
+} 
+
+// responsiveHandler()
+
 console.info('Script loaded ;)')
