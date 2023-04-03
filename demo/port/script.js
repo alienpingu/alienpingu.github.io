36c434595cb2f1fd7f4a6e256b58bf096655606e
alienpingu <42205141+alienpingu@users.noreply.github.com>
2023-04-03 19:35:30 +0200

    Add files via upload

--- demo/port/script.js
+++ demo/port/script.js
@@ -1,1 +1,12 @@
-console.log('askjdnksadnkj');
+window.onload = function() {
+    setTimeout(function() { showSession("session-1"); }, 300);
+    setTimeout(function() { showSession("session-2"); }, 400);
+    setTimeout(function() { showSession("session-3"); }, 450);
+    setTimeout(function() { showSession("session-4"); }, 870);
+    setTimeout(function() { showSession("session-5"); }, 1500);
+    setTimeout(function() { document.querySelector('#start_screen').style.display = 'none' }, 2000);
+};
+function showSession(id) {
+    const session = document.getElementById(id);
+    session.style.display = "block";
+}
