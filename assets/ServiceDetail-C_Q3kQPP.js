5dfb0481dc4d0de2913729c3532db1ca57e6dab4
alienpingu <alienpingu@users.noreply.github.com>
2026-05-16 16:56:10 +0000

    deploy: 7cec6f92665417c55145d0f7b533c2d0a5f68270

--- assets/ServiceDetail-C_Q3kQPP.js
+++ assets/ServiceDetail-C_Q3kQPP.js
@@ -1,4 +1,4 @@
-import{j as e}from"./three-CXzkpThK.js";import{d as h,L as l}from"./vendor-DjhLexnn.js";import{c as n,b as u,u as f,g as j,L as r,A as g}from"./index-DvEVLtu4.js";import{u as N}from"./use-page-meta-DtcMd6mR.js";/**
+import{n as e}from"./three-B1uFfZqc.js";import{d as h,L as l}from"./vendor-BTzTQPJw.js";import{c as n,a as u,u as f,g as j,L as r,A as g}from"./index-xgi8iCJy.js";import{u as N}from"./use-page-meta-BTez01-b.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
